238206bafd98f379caa6e9b2d03c52611955a7b0
Aditya Mukhopadhyay <webmaster@adityamukho.com>
2020-04-04 13:08:43 +0530

    Better formatting for console reporter.

--- dist/reporters/ConsoleReporter.js
+++ dist/reporters/ConsoleReporter.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, '__esModule', { value: true })
 
 class ConsoleReporter {
   report (traces) {
-    console.dir(traces, { depth: null })
+    console.log('%O', traces)
   }
 }
 
--- src/reporters/ConsoleReporter.ts
+++ src/reporters/ConsoleReporter.ts
@@ -3,6 +3,6 @@ import SpanData from '../SpanData';
 
 export default class ConsoleReporter implements Reporter {
     report(traces: [[SpanData]]): void {
-        console.dir(traces, { depth: null });
+        console.log('%O', traces);
     }
 }
