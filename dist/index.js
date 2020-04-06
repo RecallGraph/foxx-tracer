6dbb2675c7675affcb2dae007f909ffbfef7f181
Aditya Mukhopadhyay <webmaster@adityamukho.com>
2020-04-06 18:05:05 +0530

    Correctly exporting utils.

--- dist/index.js
+++ dist/index.js
@@ -6,8 +6,9 @@ const FoxxSpan_1 = require("./opentracing-impl/FoxxSpan");
 exports.FoxxSpan = FoxxSpan_1.default;
 const FoxxTracer_1 = require("./opentracing-impl/FoxxTracer");
 exports.FoxxTracer = FoxxTracer_1.default;
-const Util_1 = require("./helpers/Util");
+const Util_1 = require("./helpers/Util"), HeaderSchemas = Util_1;
 exports.Util = Util_1.default;
+exports.HeaderSchemas = HeaderSchemas;
 const reporters = require("./reporters");
 exports.reporters = reporters;
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,20 @@ import FoxxContext from './opentracing-impl/FoxxContext';
 import FoxxSpan from './opentracing-impl/FoxxSpan';
 import FoxxTracer from './opentracing-impl/FoxxTracer';
 import { Context, default as SpanData, Log, Reference, Tags, TagValue } from './helpers/SpanData';
-import Util from "./helpers/Util";
+import Util, * as HeaderSchemas from './helpers/Util';
 import * as reporters from './reporters';
 
-export { FoxxTracer, FoxxSpan, FoxxContext, SpanData, Context, Reference, Tags, TagValue, Log, Util, reporters };
+export {
+    FoxxTracer,
+    FoxxSpan,
+    FoxxContext,
+    SpanData,
+    Context,
+    Reference,
+    Tags,
+    TagValue,
+    Log,
+    Util,
+    HeaderSchemas,
+    reporters
+};
