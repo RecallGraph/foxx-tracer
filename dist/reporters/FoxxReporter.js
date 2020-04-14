4816918a62c763056fb343fa05cfec86f3962aa4
Aditya Mukhopadhyay <webmaster@adityamukho.com>
2020-04-14 19:36:19 +0530

    Removed task wrapper.

--- dist/reporters/FoxxReporter.js
+++ dist/reporters/FoxxReporter.js
@@ -1,23 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const tasks = require('@arangodb/tasks');
 class FoxxReporter {
     report(traces) {
         const spans = traces.flat();
         console.debug(spans);
-        // noinspection TypeScriptValidateJSTypes
-        const task = tasks.register({
-            command: function (spans) {
-                try {
-                    module.context.dependencies.traceCollector.recordSpans(spans);
-                }
-                catch (e) {
-                    console.error(`Collector endpoint error: ${e}`);
-                }
-            },
-            params: spans
-        });
-        console.debug(task);
+        module.context.dependencies.traceCollector.recordSpans(spans);
     }
 }
 exports.default = FoxxReporter;
--- src/reporters/FoxxReporter.ts
+++ src/reporters/FoxxReporter.ts
@@ -1,24 +1,11 @@
 import Reporter from './Reporter';
 import SpanData from '../helpers/SpanData';
 
-const tasks = require('@arangodb/tasks');
-
 export default class FoxxReporter implements Reporter {
   report(traces: [[SpanData]]): void {
     const spans = traces.flat();
     console.debug(spans);
 
-    // noinspection TypeScriptValidateJSTypes
-    const task = tasks.register({
-      command: function (spans) {
-        try {
-          module.context.dependencies.traceCollector.recordSpans(spans);
-        } catch (e) {
-          console.error(`Collector endpoint error: ${e}`);
-        }
-      },
-      params: spans
-    });
-    console.debug(task);
+    module.context.dependencies.traceCollector.recordSpans(spans);
   }
 }
