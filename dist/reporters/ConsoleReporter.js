1eb0dab33060020f3b2b05d4655f6d6550f7f54a
Aditya Mukhopadhyay <webmaster@adityamukho.com>
2020-04-15 11:25:42 +0530

    Better formatting for console reporter.

--- dist/reporters/ConsoleReporter.js
+++ dist/reporters/ConsoleReporter.js
@@ -3,14 +3,15 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const util_1 = require("util");
 class ConsoleReporter {
     report(traces) {
-        console.log(util_1.inspect(traces, {
-            depth: Infinity,
-            maxArrayLength: Infinity,
-            breakLength: Infinity,
-            compact: true,
-            sorted: true
-        }));
+        console.log(util_1.inspect(traces, ConsoleReporter.FORMAT_OPTIONS));
     }
 }
 exports.default = ConsoleReporter;
+ConsoleReporter.FORMAT_OPTIONS = {
+    depth: Infinity,
+    maxArrayLength: Infinity,
+    breakLength: Infinity,
+    compact: true,
+    sorted: true
+};
 //# sourceMappingURL=ConsoleReporter.js.map
--- src/reporters/ConsoleReporter.ts
+++ src/reporters/ConsoleReporter.ts
@@ -3,13 +3,15 @@ import SpanData from '../helpers/SpanData';
 import { inspect } from 'util';
 
 export default class ConsoleReporter implements Reporter {
+  private static readonly FORMAT_OPTIONS = {
+    depth: Infinity,
+    maxArrayLength: Infinity,
+    breakLength: Infinity,
+    compact: true,
+    sorted: true
+  };
+
   report(traces: [[SpanData]]): void {
-    console.log(inspect(traces, {
-      depth: Infinity,
-      maxArrayLength: Infinity,
-      breakLength: Infinity,
-      compact: true,
-      sorted: true
-    }));
+    console.log(inspect(traces, ConsoleReporter.FORMAT_OPTIONS));
   }
 }
